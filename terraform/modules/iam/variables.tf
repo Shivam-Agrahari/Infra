bc1e32a7e14b616bf07c418587c454df526eca01
Shivam <shivamagrahari8957@gmail.com>
2026-05-01 19:36:46 +0530

    Fix workflow path to terraform/env/prod

--- terraform/modules/iam/variables.tf
+++ terraform/modules/iam/variables.tf
@@ -1,3 +1,16 @@
-variable "role_name" {}
-variable "oidc_provider_arn" {}
-variable "github_subject" {}
+variable "role_name" {
+  description = "The name of the IAM role to be used by GitHub Actions via OIDC"
+  type        = string
+  default     = "github-actions-terraform-role"
+}
+
+variable "oidc_provider_arn" {
+  description = "ARN of the OIDC provider for GitHub Actions"
+  type        = string
+}
+
+variable "github_subject" {
+  description = "GitHub OIDC subject string (repo and branch)"
+  type        = string
+  # Example: "repo:Shivam-Agrahari/Infra:ref:refs/heads/main"
+}
