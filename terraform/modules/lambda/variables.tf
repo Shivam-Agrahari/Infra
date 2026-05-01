32b732bdb6e00fe8368cba9a02d8cbf2639bf621
Shivam <shivamagrahari8957@gmail.com>
2026-05-01 19:51:20 +0530

    Fix workflow path to terraform/env/prod

--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -1,4 +1,14 @@
+variable "sns_topic_arn" {
+  description = "ARN of the SNS topic to trigger the Lambda"
+  type        = string
+}
+
 variable "lambda_role_arn" {
   description = "IAM role ARN for Lambda execution"
+  type        = string
+}
+
+variable "eventbridge_rule_arn" {
+  description = "ARN of the EventBridge rule to trigger the Lambda"
   type        = string
 }
