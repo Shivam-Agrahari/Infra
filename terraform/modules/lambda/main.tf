1e70321c8396c1473dd19700ad7d741b5e105350
Shivam <shivamagrahari8957@gmail.com>
2026-05-01 19:48:45 +0530

    Fix workflow path to terraform/env/prod

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,22 +1,15 @@
-resource "aws_lambda_function" "notifier" {
-  function_name = "backup-failure-notifier"
-  role          = var.lambda_role_arn
-  handler       = "index.handler"
-  runtime       = "python3.9"
-
-  filename      = "${path.module}/lambda.zip"
-
-  environment {
-    variables = {
-      SNS_TOPIC_ARN = var.sns_topic_arn
-    }
-  }
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.root}/lambda"         # path to your source code folder
+  output_path = "${path.module}/lambda.zip"   # where Terraform will write the zip
 }
 
-resource "aws_lambda_permission" "allow_eventbridge" {
-  statement_id  = "AllowExecutionFromEventBridge"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.notifier.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = var.eventbridge_rule_arn
+resource "aws_lambda_function" "notifier" {
+  function_name = "notifier"
+  role          = var.lambda_role_arn
+  handler       = "index.handler"             # index.py with handler() function
+  runtime       = "python3.9"
+
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 }
