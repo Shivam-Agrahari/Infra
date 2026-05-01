9274b681b5eb9861e0cf30434f7716ca640ce13d
Shivam <shivamagrahari8957@gmail.com>
2026-05-01 20:03:31 +0530

    Fix workflow path to terraform/env/prod

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,6 +1,6 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.root}/../../INFRA/lambda"         # path to your source code folder
+  source_dir  = "${path.root}/../../Infra/lambda"         # path to your source code folder
   output_path = "${path.module}/lambda.zip"   # where Terraform will write the zip
 }
 
